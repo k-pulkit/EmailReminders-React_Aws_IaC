0075c5d24af5d7a82522439478c62064704095fe
k-pulkit <pulkit42041@gmail.com>
2024-02-28 00:00:00 +0000

    Format About page

--- frontend/reminder-app/src/routes/root/about.jsx
+++ frontend/reminder-app/src/routes/root/about.jsx
@@ -3,12 +3,16 @@ import architecture from "@assets/architecture.png";
 
 const About = () => {
   return (
-    <div className="mt-4 flex h-full flex-col items-center justify-center gap-4 p-3">
-      <h1 className="font-mono text-3xl text-primary">Architecture Diagram</h1>
-      <img
-        src={architecture}
-        className="rounded-xl p-2 outline outline-primary"
-      />
+    <div className="mb-10 mt-10 flex h-[80vh] flex-col items-center justify-center gap-4 p-3">
+      <h1 className="grow pt-10 font-serif text-4xl tracking-widest text-gray-700">
+        Architecture Diagram
+      </h1>
+      <div className="flex h-full w-full items-start justify-center">
+        <img
+          src={architecture}
+          className="h-[92%] w-full rounded-xl object-contain object-center outline outline-black"
+        />
+      </div>
     </div>
   );
 };
